dae45e7c7076ee07f71275734909714808e01bc9
eileen17026 <166679671+eileen17026@users.noreply.github.com>
2024-06-02 18:16:14 +0800

    Add files via upload

---
+++ 4.9.1.py
@@ -0,0 +1,15 @@
+def compare_value(x, y):
+    if x > y:
+        return 1
+    if x == y:
+        return 0
+    if x < y:
+        return -1
+
+# 輸入兩個數字
+x = float(input("x="))
+y = float(input("y="))
+
+# 比較兩個數字並輸出結果
+result = compare_value(x, y)
+print("結果：", result)
